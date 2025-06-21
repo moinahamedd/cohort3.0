287cb3276bf9e06ae843218e44f19cc0b2765fd1
Aham3d <ahamedjust@gmail.com>
2025-06-21 08:32:34 +0530

    alernative solution without callback hell for the same exaple

--- week2/week2.3/index.js
+++ week2/week2.3/index.js
@@ -1,24 +1,41 @@
 // callback hell examples with setTimeout()
 
-setTimeout( function () {
+// setTimeout( function () {
+//     console.log('hi');
+//     setTimeout(function () {
+//         console.log('hello');
+//         setTimeout(function () {
+//             console.log('last one');
+//         }, 5000)
+//     }, 3000);
+// }, 1000);
+
+
+// setTimeout( function () {
+//     console.log('hi'); 
+// }, 1000);
+
+// setTimeout( function () {
+//     console.log('hello');
+// }, 3000 + 1000)
+
+// setTimeout( function () {
+//     console.log('last one');
+// }, 4000 + 5000);
+
+// alter solution without callback hell
+function step1() {
     console.log('hi');
-    setTimeout(function () {
-        console.log('hello');
-        setTimeout(function () {
-            console.log('last one');
-        }, 5000)
-    }, 3000);
-}, 1000);
+    setTimeout(step2, 3000);
+}
 
-
-setTimeout( function () {
-    console.log('hi'); 
-}, 1000);
-
-setTimeout( function () {
+function step2() {
     console.log('hello');
-}, 3000 + 1000)
+    setTimeout(step3, 5000);
+}
 
-setTimeout( function () {
+function step3() {
     console.log('last one');
-}, 4000 + 5000);
+}
+
+setTimeout(step1, 1000);
