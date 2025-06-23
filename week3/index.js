be3a7d3acdfd761eaa48da7062defd3a395a9f08
Aham3d <ahamedjust@gmail.com>
2025-06-23 22:30:34 +0530

    deleting el - parentNode, removeChild(el)

--- week3/index.js
+++ week3/index.js
@@ -1,8 +1,14 @@
-let ctr = 0; 
+// let ctr = 0; 
 
-function callback() {
-    document.querySelectorAll('h2')[1].innerHTML = ctr;
-    ctr = ctr + 1;
+// function callback() {
+//     document.querySelectorAll('h2')[1].innerHTML = ctr;
+//     ctr = ctr + 1;
+// }
+
+// setInterval(callback, 1000);
+
+function deleteTask() {
+    const ele = document.querySelector('h2');
+    const parentele = ele.parentNode;
+    parentele.removeChild(ele);
 }
-
-setInterval(callback, 1000);
